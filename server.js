002aeec1cc33dbfce342f3fb32dfeaec4fe42fcc
berkforbes <berkforbes@yahoo.com>
2022-01-21 00:03:44 -0700

    middleware, findbyID, post function working

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
+const notes  = require('./db/db.json');
 var express = require('express');
 var app = express();
-const notes  = require('./db/db.json');
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+
 
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
-app.post('/api/notes', (req, res) => {
-  res.json(notes);
-});
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+  app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+  });
+
 
 app.listen(3002, () => {
   console.log(`API server now on port 3002!`);
 });
+
